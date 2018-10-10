138641b2cf354188e3ad8980827d323363adec4d
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-10-10 10:22:06 +0100

    Spring Initializr skeleton with Web, JPA etc.

---
+++ src/main/java/uk/org/esciencelab/researchobjectservice/ResearchObjectServiceApplication.java
@@ -0,0 +1,12 @@
+package uk.org.esciencelab.researchobjectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchObjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchObjectServiceApplication.class, args);
+	}
+}
