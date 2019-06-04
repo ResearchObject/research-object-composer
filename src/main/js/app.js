9f3ecf45b0f0b539483ca714f5eea15716698ed2
Finn Bacall <finn.bacall@manchester.ac.uk>
2019-06-04 15:50:30 +0100

    Restructuring. Show provisional table of ROs

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,24 +2,38 @@ require('babel-polyfill');
 const React = require('react');
 const ReactDOM = require('react-dom');
 import Form from "react-jsonschema-form";
-const jsonpointer = require('jsonpointer');
 const client = require('./client');
+const schemaUtil = require('./schema');
+const follow = require('./follow');
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {profiles: []};
+        this.state = { profiles: [], researchObjects: []};
     }
 
     componentDidMount() {
-        client({method: 'GET', path: '/profiles'}).done(response => {
-            this.setState({profiles: response.entity._embedded.researchObjectProfileList});
-        });
+        this.loadData();
+    }
+
+    loadData() {
+        // TODO: Refactor API so "/profiles" and "/research_objects" are discoverable from root
+        follow(client, '/profiles', [])
+            .done(profileCollection => {
+                this.setState({ profiles: profileCollection.entity._embedded.researchObjectProfileList });
+            });
+        follow(client, '/research_objects', [])
+            .done(researchObjectCollection => {
+                this.setState({ researchObjects: researchObjectCollection.entity._embedded.researchObjectSummaryList });
+            });
     }
 
     render() {
         return (
-            <ProfileList profiles={this.state.profiles}/>
+            <div>
+              <ProfileList profiles={this.state.profiles}/>
+              <ResearchObjectList researchObjects={this.state.researchObjects}/>
+            </div>
         )
     }
 }
@@ -30,124 +44,96 @@ class ProfileList extends React.Component{
             <Profile key={profile._links.self.href} profile={profile}/>
         );
         return (
-            <table>
-                <tbody>
-                <tr>
-                    <th>Name</th>
-                    <th>Schema</th>
-                </tr>
-                {profiles}
-                </tbody>
-            </table>
+            <div className="profile-list-wrapper">
+                <h2>Available Profiles</h2>
+                <div className="profile-list">
+                    {profiles}
+                </div>
+            </div>
         )
     }
 }
 
 class Profile extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { form: null };
+        this.loadForm = this.loadForm.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="profile">
+                <h3 onClick={this.loadForm}>{this.props.profile.name}</h3>
+                <a href={this.props.profile._links.schema.href} target="_blank">{this.props.profile._links.schema.href}</a>
+                {this.state.form}
+            </div>
+        )
+    }
+
+    loadForm() {
+        const schemaHref = this.props.profile._links.schema.href;
+        schemaUtil.loadSchema(schemaHref).then((schema) => {
+            const resolved = schemaUtil.resolveSchema(schemaHref, schema);
+            const uiSchema = Object.assign(defaultUiSchema, schemaUtil.buildUiSchema(resolved));
+
+            this.setState({
+                form: <ResearchObjectForm schema={resolved} uiSchema={uiSchema} onSubmit={this.onSubmit}/>
+            });
+        });
+    }
+
+    onSubmit(x) {
+        console.log(x);
+    }
+}
+
+class ResearchObjectList extends React.Component{
+    render() {
+        const researchObjectSummaries = this.props.researchObjects.map(researchObject =>
+            <ResearchObjectSummary key={researchObject._links.self.href} researchObject={researchObject}/>
+        );
+        return (
+            <div className="research-object-list-wrapper">
+                <h2>Research Objects</h2>
+                <table className="table research-object-list">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Link</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {researchObjectSummaries}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+class ResearchObjectSummary extends React.Component{
     render() {
         return (
             <tr>
-                <td>{this.props.profile.name}</td>
-                <td><a href={this.props.profile._links.schema.href} target="_blank">{this.props.profile._links.schema.href}</a></td>
+                <td>{this.props.researchObject.id}</td>
+                <td><a href={this.props.researchObject._links.self.href} target="_blank">{this.props.researchObject._links.self.href}</a></td>
             </tr>
         )
     }
 }
 
-const cache = {};
-const pendingLoads = {};
-const schemaCache = {};
-
-window.cache = cache;
-window.schemaCache = schemaCache;
-
-// Deep-load the schema, and any referenced schemas, from the given URL.
-function loadSchema(path) {
-    if (pendingLoads[path]) {
-        console.log("Load pending:", path);
-        return pendingLoads[path];
+class ResearchObjectForm extends React.Component{
+    render() {
+        return (
+            <Form schema={this.props.schema}
+                  uiSchema={this.props.uiSchema}
+                  // onChange={log("changed")}
+                  onSubmit={this.props.onSubmit}
+                  // onError={log("errors")}
+            />
+        )
     }
-    console.log("Loading:", path);
-    pendingLoads[path] = fetch(path).then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        return discoverRefs(path, json, json).then(function () {
-            cache[path] = json;
-            return json;
-        });
-    });
-
-    return pendingLoads[path];
-}
-
-
-// Iterate over every property on the given object, and load any schemas referenced by $refs.
-function discoverRefs(rootPath, root, obj) {
-    const promises = [];
-    for (const property in obj) {
-        if (property === "$ref") {
-            const parts = obj[property].split("#");
-            const url = parts[0];
-            if (url !== "") {
-                promises.push(loadSchema(url));
-            }
-        }
-        if (typeof obj[property] === 'object') {
-            promises.push(discoverRefs(rootPath, root, obj[property]));
-        }
-    }
-
-    return Promise.all(promises);
-}
-
-function fetchRef(schemaUrl, pointer) {
-    return jsonpointer.get(cache[schemaUrl], pointer);
-}
-
-// "Flatten" a schema by inlining any $refs.
-function replaceRefs(context, obj) {
-    if (obj["$ref"]) {
-        const parts = obj["$ref"].split("#");
-        const pointer = parts[1];
-        const innerContext = parts[0] === "" ? context : parts[0];
-        return replaceRefs(innerContext, fetchRef(innerContext, pointer, obj["$ref"]));
-    }
-
-    const newObj = {};
-    for (const property in obj) {
-        if (typeof obj[property] === 'object' && obj[property] !== null) {
-            if (Array.isArray(obj[property])) {
-                newObj[property] = obj[property].map((v) => replaceRefs(context, v));
-            } else {
-                newObj[property] = replaceRefs(context, obj[property]);
-            }
-        } else {
-            newObj[property] = obj[property];
-        }
-    }
-
-    return newObj;
-}
-
-// Create uiSchema
-function buildUiSchema(obj) {
-    const uiSchema = obj['$ui'] || {};
-
-    if (obj.type === 'object' && obj.properties) {
-        for (const property in obj.properties) {
-            const propUiSchema = buildUiSchema(obj.properties[property]);
-            if (Object.keys(propUiSchema).length) {
-                uiSchema[property] = propUiSchema;
-            }
-        }
-    } else if (obj.type === 'array' && obj.items) {
-        const itemSchema = buildUiSchema(obj.items);
-        if (Object.keys(itemSchema).length) {
-            uiSchema.items = itemSchema;
-        }
-    }
-
-    return uiSchema;
 }
 
 const defaultUiSchema = {
@@ -172,27 +158,7 @@ const defaultUiSchema = {
     workflow_params: { 'ui:widget': 'textarea', 'ui:options': { rows: 5 } }
 };
 
-window.res = {};
-
-loadSchema("/schemas/draft_task.schema.json").then(function (schema) {
-    const resolved = replaceRefs("/schemas/draft_task.schema.json", schema);
-    const uiSchema = Object.assign(defaultUiSchema, buildUiSchema(resolved));
-    window.resolvedSchema = resolved;
-    window.uiSchema = uiSchema;
-
-    const log = (type) => console.log.bind(console, type);
-
-    ReactDOM.render(
-        <Form schema={resolved}
-              uiSchema={uiSchema}
-              onChange={log("changed")}
-              onSubmit={log("submitted")}
-              onError={log("errors")} />
-        , document.getElementById("form")
-    );
-
-    ReactDOM.render(
-        <App />,
-        document.getElementById('react')
-    );
-});
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+);
