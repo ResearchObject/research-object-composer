97bca6d85b824e971ccd4371712e4a5f465cd86d
Finn Bacall <finn.bacall@manchester.ac.uk>
2019-06-05 13:49:03 +0100

    Add some extra metadata/links to RO summary object

--- src/main/java/uk/org/esciencelab/researchobjectservice/researchobject/ResearchObjectSummaryResourceAssembler.java
+++ src/main/java/uk/org/esciencelab/researchobjectservice/researchobject/ResearchObjectSummaryResourceAssembler.java
@@ -3,6 +3,7 @@ package uk.org.esciencelab.researchobjectservice.researchobject;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.stereotype.Component;
+import uk.org.esciencelab.researchobjectservice.profile.ResearchObjectProfileController;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -17,7 +18,8 @@ public class ResearchObjectSummaryResourceAssembler implements ResourceAssembler
     public Resource<ResearchObjectSummary> toResource(ResearchObject researchObject) {
         ResearchObjectSummary summary = new ResearchObjectSummary(researchObject);
         Resource<ResearchObjectSummary> resource = new Resource<>(summary,
-                linkTo(methodOn(ResearchObjectController.class).one(researchObject.getId())).withSelfRel());
+                linkTo(methodOn(ResearchObjectController.class).one(researchObject.getId())).withSelfRel(),
+                linkTo(methodOn(ResearchObjectProfileController.class).one(researchObject.getProfileName())).withRel("profile"));
 
         return resource;
     }
